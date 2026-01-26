5ebd479ad7964b8a094c2e84829f8b401408ea33
aganyo360 <wilsonaganyo@gmail.com>
2026-01-26 16:31:12 +0300

    update service/serializer

--- services/serializers.py
+++ services/serializers.py
@@ -8,8 +8,7 @@ class ServiceSerializer(serializers.ModelSerializer):
             'id', 'name', 'description', 'organization', 'city', 'address',
             'latitude', 'longitude', 'accessibility', 'created_by', 'created_at'
         ]
-        read_only_fields = ['created_by', 'created_at']
-        
+        read_only_fields = ['created_by', 'created_at']        
 
     def create(self, validated_data):
         validated_data['created_by'] = self.context['request'].user
